255c91d5e079b22ea510fecb5630df58a6f50ae7
Sergio Vidal <svidal1987@gmail.com>
2022-10-18 19:48:53 -0300

    desarrollo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,54 +1,34 @@
 export const environment = {
-    production: true,
-    //AS
-
-    primero_pdf1:'./assets/pdf/64a25ce6fe9b818f7c78bf7be0f607bd.pdf',
-    primero_pdf2:'./assets/txt/1ASetapa3.txt',
-    primero_password:[
-      'WñNtLkQr',
-      'FñUtLmWk',
-      'LtpQiHSQ',
-      'HeFmOtYm',
-      'QUQrUxrN',
-      'kYvrNJiñ',
-      'cgQQJñJF'],
-
-    segundo_pdf1:'./assets/pdf/7b6583f7ddee4736f7206a879056c702.pdf',
-    segundo_pdf2:'./assets/pdf/db4d5e9af467fb4045a751e5cdf94007.pdf',
-    segundo_pdf3:'./assets/pdf/f5044f234292d2f1806def2581ab5db1.pdf',
-    segundo_pdf4:'./assets/json/2ASetapa3.json',
-    segundo_password:'1010111111',
-    
-    tercero_pdf1:'./assets/pdf/a477c991addeeae361993fb89b31155b.pdf',
-    tercero_pdf2:'./assets/pdf/c3b42b1b93a5961d016398c80584e247.pdf',
-    tercero_pdf3:'./assets/pdf/c205ccfa8dbd96126c47719e4aa5adb9.pdf',
-    tercero_password:'1011011110',
+  production: true,
+  //AS
   
-    //TSH
-    basico_pdf1:'./assets/pdf/64bf9eb7f9ac8fe7b017c8d1fc7bad27.pdf',
-    basico_pdf2:'./assets/pdf/e7431eb5415aaf3f09697ce12524037c.pdf',
-    basico_password:'1511422010',
+  primero_pdf1:'./assets/pdf/64a25ce6fe9b818f7c78bf7be0f607bd.pdf',
+  primero_pdf2:'./assets/txt/1ASetapa3.txt',
+  primero_password:[
+    'WñNtLkQr',
+    'FñUtLmWk',
+    'LtpQiHSQ',
+    'HeFmOtYm',
+    'QUQrUxrN',
+    'kYvrNJiñ',
+    'cgQQJñJF'],
 
-    avanzado_pdf1:'./assets/pdf/22bccd3d0fd48f62573a792118739384.pdf',
-    avanzado_pdf2:'./assets/pdf/d75f50ecd58ebc3c3dc3a5adf89fccdb.pdf',
-    avanzado_password:'15114220102',
 
-
-
-
-    errors_gif:[
-      './assets/video/dario-barassi.gif',
-      './assets/video/dario-barassi.gif',
-      './assets/video/tony-stark-iron-man.gif',
-      './assets/video/bsod.gif',
-      './assets/video/nopes-nopez.gif',
-      './assets/video/saquontroll-saquonjudge26.gif',
-      './assets/video/risa-dario-barassi.gif',
-      './assets/video/toy-story-woody.gif',
-      './assets/video/oh-nooo-facepalm-baby.gif',
-      './assets/video/rocksideroad-snowball.gif',
-      './assets/video/tenor.gif'
-      
-    ]
+  susses_gif:[
+    
+    './assets/video/bh187-waynes-world.gif',
+    './assets/video/congratulations-congrats.gif',
+    './assets/video/electro-disco.gif',
+    './assets/video/gif-party.gif',
+    './assets/video/gifsoup-funny.gif',
+    './assets/video/leonardo-dicaprio.gif',
+    './assets/video/minions.gif',
+    './assets/video/ok-all.gif',
+    './assets/video/sacha-baron.gif',
+    './assets/video/snoop-dogg-rap.gif',
+    './assets/video/yes-baby.gif',
+    './assets/video/yessir.gif'
+  ]
 
 };
+
